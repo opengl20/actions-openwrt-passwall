dd9f703e3b6e8a2eca2605b6bbc5baebb81703ad
ycg31 <ycg31@qq.com>
2020-03-30 23:57:20 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=================================================
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.100.222/g' openwrt/package/base-files/files/bin/config_generate
+#2. Modify default gatewy
+sed -i '108a \                                set network.$1.gateway='192.168.100.1'' openwrt/package/base-files/files/bin/config_generate
+#3. Modify default DNS
+sed -i '109a \                                set network.$1.dns='222.172.200.68\ 223.5.5.5\ 8.8.8.8'' openwrt/package/base-files/files/bin/config_generate
+#4. Add firmware timestamp
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d-%H%M%S)-/g' openwrt/include/image.mk
